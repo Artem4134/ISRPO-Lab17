b0354cc3f91a4c04b67e5019bb71dd6c8be47031
Artem4134 <artem.ponomarencko2018@yandex.ru>
2026-03-30 17:48:45 +0300

    Step 6: while and for loops with flowcharts

---
+++ code/Step6_MultiplicationTable/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.WriteLine("Таблицв умножения");
+Console.Write("Введите число для таблицы умножения: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Таблица умножения для {number}: ");
+for (int i = 1; i <= 10; i++)
+{
+    int result = number * i;
+    System.Console.WriteLine($"{number} x {i} = {result}");
+}
+Console.WriteLine("Конец программы");
